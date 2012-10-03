e844ddb25ef497816ffab358a7bd2dc6cf944fee
nodemaker <nodemaker@gmail.com>
2012-10-03 14:56:16 -0400

    Now setting root view controller

--- src/Three20UINavigator/Sources/TTBaseNavigator.m
+++ src/Three20UINavigator/Sources/TTBaseNavigator.m
@@ -256,6 +256,7 @@ __attribute__((weak_import));
       [_rootContainer navigator:self setRootViewController:_rootViewController];
 
     } else {
+      [self.window setRootViewController:_rootViewController];
       [self.window addSubview:_rootViewController.view];
     }
   }
